5e790148e362a0552887664d979ef62a5626aadc
Markus328 <markus328@tutanota.com>
2023-06-13 23:51:49 -0300

    Added overlay

--- flake.nix
+++ flake.nix
@@ -1,27 +1,41 @@
 {
   description = "The 64gram-desktop telegram client, binary version";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-  outputs = { self, nixpkgs }: 
-  let data = rec {
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    t64desktop = import ./t64desktop.nix { pkgs = pkgs; };
-    fhs = import ./fhs.nix {pkgs = pkgs; t64desktop = t64desktop;};
-    };
-  in with data;
-  rec {
-    packages.x86_64-linux =
-    {
-      default = import ./default.nix { inherit pkgs; inherit t64desktop; inherit fhs; };
-    };
-      
-    apps.x86_64-linux = let
-      app = name: {
-        type = "app";
-        program = "${packages.x86_64-linux.${name}}/bin/telegram-desktop";
+  outputs = inputs: let
+    data = rec {
+      pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+      t64desktop = import ./t64desktop.nix {pkgs = pkgs;};
+      fhs = import ./fhs.nix {
+        pkgs = pkgs;
+        t64desktop = t64desktop;
       };
-    in
-    {
-      default = app "default";
     };
-  };
+  in
+    with data; rec {
+      packages.x86_64-linux = {
+        default = import ./default.nix {
+          inherit pkgs;
+          inherit t64desktop;
+          inherit fhs;
+        };
+      };
+
+      defaultPackage.x86_64-linux = packages.x86_64-linux.default;
+
+      apps.x86_64-linux = let
+        app = name: {
+          type = "app";
+          program = "${packages.x86_64-linux.${name}}/bin/telegram-desktop";
+        };
+      in {
+        default = app "default";
+      };
+
+      overlay = overlays.default;
+      overlays.default = final: prev: {
+        t64gram = import ./t64desktop.nix {
+          pkgs = final;
+        };
+      };
+    };
 }
