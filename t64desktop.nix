249378b70bc59f9cfb8f40fbc9146fc61c57e9f4
Markus328 <markus328@tutanota.com>
2023-06-20 22:54:20 -0300

    formated code

--- t64desktop.nix
+++ t64desktop.nix
@@ -1,6 +1,5 @@
-{ pkgs, ...}:
-with pkgs;
-let
+{pkgs, ...}:
+with pkgs; let
   url_git = "https://github.com/TDesktop-x64/tdesktop";
   version = "1.0.85";
   srcs = [
@@ -21,94 +20,94 @@ let
   icon = builtins.elemAt srcs 1;
   app = builtins.elemAt srcs 2;
 
-
   rpath = pkgs.lib.makeLibraryPath [
-
-        glib
-				fontconfig
-				freetype
-				libglvnd
-				xorg.libxcb
-				xorg.libX11 
-        dbus
-				xkeyboard_config
-				desktop-file-utils
-				wayland
-        qt6.qtbase
-        qt6.qtwayland
-        qt6.qtsvg
-		    qt6.qtimageformats
-		    qt6.qt5compat
-        glibmm
-		    gtk3
-		    libdbusmenu
-		    lz4
-		    xxHash
-		    ffmpeg
-		    openalSoft
-		    minizip
-		    libopus
-		    alsa-lib
-		    libpulseaudio
-		    pipewire
-		    range-v3 tl-expected
-		    hunspell
-		    glibmm_2_68
-		    webkitgtk_6_0
-		    jemalloc
-		    rnnoise
-		    protobuf
-		    util-linuxMinimal 
-		    pcre
-		    xorg.libpthreadstubs
-		    xorg.libXdamage
-		    xorg.libXdmcp
-		    libselinux
-		    libsepol
-		    libepoxy
-		    at-spi2-core
-		    xorg.libXtst
-		    libthai
-		    libdatrie
-		    libsysprof-capture
-		    libpsl
-        brotli
-        microsoft_gsl
-        rlottie
+    glib
+    fontconfig
+    freetype
+    libglvnd
+    xorg.libxcb
+    xorg.libX11
+    dbus
+    xkeyboard_config
+    desktop-file-utils
+    wayland
+    qt6.qtbase
+    qt6.qtwayland
+    qt6.qtsvg
+    qt6.qtimageformats
+    qt6.qt5compat
+    glibmm
+    gtk3
+    libdbusmenu
+    lz4
+    xxHash
+    ffmpeg
+    openalSoft
+    minizip
+    libopus
+    alsa-lib
+    libpulseaudio
+    pipewire
+    range-v3
+    tl-expected
+    hunspell
+    glibmm_2_68
+    webkitgtk_6_0
+    jemalloc
+    rnnoise
+    protobuf
+    util-linuxMinimal
+    pcre
+    xorg.libpthreadstubs
+    xorg.libXdamage
+    xorg.libXdmcp
+    libselinux
+    libsepol
+    libepoxy
+    at-spi2-core
+    xorg.libXtst
+    libthai
+    libdatrie
+    libsysprof-capture
+    libpsl
+    brotli
+    microsoft_gsl
+    rlottie
   ];
 in
-stdenv.mkDerivation {
-  pname = "64gram-desktop-bin";
-  inherit version srcs;
-  dontBuild = true;
-  nativeBuildInputs = [ 
-        unzip 
-        qt6.qtbase qt6.wrapQtAppsHook
-        # autoPatchelfHook
-  ];
+  stdenv.mkDerivation {
+    pname = "64gram-desktop-bin";
+    inherit version srcs;
+    dontBuild = true;
+    nativeBuildInputs = [
+      unzip
+      qt6.qtbase
+      qt6.wrapQtAppsHook
+      # autoPatchelfHook
+    ];
 
-  # runtimeDependencies = [
+    # runtimeDependencies = [
 
-  # ];
-  
-  unpackPhase = ''
-    unzip ${app}
-  '';
-  installPhase = ''
-    mkdir -p $out/bin 
-    mkdir -p $out/share/pixmaps 
-    mkdir -p $out/share/applications 
-    mkdir -p $out/share/icons/hicolor/256x256/apps
+    # ];
 
-    cp ./Telegram $out/bin/telegram-desktop
-    cp ${icon} $out/share/pixmaps/telegram.png
-    cp ${icon} $out/share/icons/hicolor/256x256/apps/telegram.png
+    unpackPhase = ''
+      unzip ${app}
+    '';
+    installPhase = ''
+      mkdir -p $out/bin
+      mkdir -p $out/share/pixmaps
+      mkdir -p $out/share/applications
+      mkdir -p $out/share/icons/hicolor/256x256/apps
 
-    cp ${desktop} $out/share/applications/telegram.desktop
-    sed -i 's/@CMAKE_INSTALL_FULL_BINDIR@\///g' $out/share/applications/telegram.desktop
-  '';
-  postFixup = ''
+      cp ./Telegram $out/bin/telegram-desktop
+      cp ${icon} $out/share/pixmaps/telegram.png
+      cp ${icon} $out/share/icons/hicolor/256x256/apps/telegram.png
+
+      cp ${desktop} $out/share/applications/telegram.desktop
+      sed -i 's/@CMAKE_INSTALL_FULL_BINDIR@\///g' $out/share/applications/telegram.desktop
+    '';
+    postFixup = ''
       patchelf --set-interpreter ${pkgs.glibc}/lib/ld-linux-x86-64.so.2 "$out/bin/telegram-desktop"
       patchelf --set-rpath ${rpath} "$out/bin/telegram-desktop"
-  '';
-}
+    '';
+  }
