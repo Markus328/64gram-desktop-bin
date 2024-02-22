e9391fc3b76843704f76b48d0b51f80590ecb6cb
Markus328 <markus328@tutanota.com>
2024-02-22 20:31:14 -0300

    Using nosys instead of flake-utils

--- flake.nix
+++ flake.nix
@@ -2,38 +2,29 @@
   description = "The 64gram-desktop telegram client, binary version";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-utils = {
-      url = "github:numtide/flake-utils";
+    nosys = {
+      url = "github:divnix/nosys";
     };
   };
-  outputs = {
+  outputs = inputs @ {
+    self,
     nixpkgs,
-    flake-utils,
+    nosys,
     ...
   }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-      };
+    nosys inputs ({
+      self,
+      nixpkgs,
+      ...
+    }: let
+      inherit (nixpkgs.legacyPackages) pkgs;
       t64desktop = import ./t64desktop.nix {inherit pkgs;};
-    in rec {
+    in {
       packages = {
         default = t64desktop;
       };
 
-      # defaultPackage.x86_64-linux = packages.x86_64-linux.default;
-
-      # apps.x86_64-linux = let
-      #   app = name: {
-      #     type = "app";
-      #     program = "${packages.x86_64-linux.${name}}/bin/telegram-desktop";
-      #   };
-      # in {
-      #   default = app "default";
-      # };
-
-      overlay = overlays.default;
-      overlays.default = final: prev: {
+      _overlays.default = final: prev: {
         t64gram = t64desktop;
       };
     });
