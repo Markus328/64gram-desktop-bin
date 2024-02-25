aac33f1b3e271c9d58b4d1860c48a2b6cbd99ec2
Markus328 <markus328@tutanota.com>
2024-02-25 16:16:06 -0300

    Removed nosys dependency
    Since this binary of 64gram is for x86_64-linux, I don't need use nosys or flake-utils.lib.eachDefaultSystem. Both approachs were giving errors about overlays, so I gave up and hardcoded the system. For this single flake I think it's good since there are no way it could work if not on x86_64-linux and I won't support darwin (for now)

--- flake.nix
+++ flake.nix
@@ -2,32 +2,28 @@
   description = "The 64gram-desktop telegram client, binary version";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    nosys = {
-      url = "github:divnix/nosys";
-    };
   };
   outputs = inputs @ {
     self,
     nixpkgs,
-    nosys,
     ...
-  }:
-    nosys inputs ({
-      self,
-      nixpkgs,
-      ...
-    }: let
-      inherit (nixpkgs.legacyPackages) pkgs;
-      t64desktop = import ./t64desktop.nix {inherit pkgs;};
-    in {
-      packages = {
-        default = t64desktop;
-      };
+  }: let
+    # The only supported system
+    system = "x86_64-linux";
+    inherit (nixpkgs.legacyPackages.${system}) pkgs;
+    t64desktop = import ./t64desktop.nix {inherit pkgs;};
+  in {
+    packages.${system} = {
+      default = t64desktop;
+    };
+    apps.${system}.default = {
+      type = "app";
+      program = "${self.packages.${system}.default}/bin/telegram-desktop";
+    };
 
-      #ADD THIS NEXT
-      #_overlay = self.overlays.default;
-      _overlays.default = final: prev: {
-        t64gram = t64desktop;
-      };
-    });
+    overlay = self.overlays.default;
+    overlays.default = final: prev: {
+      t64gram = t64desktop;
+    };
+  };
 }
